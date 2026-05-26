0daaa873347cae27954ffd55524a895678d59632
Flixie <alalseidel@gmail.com>
2026-05-26 16:03:32 +0100

    Added Initial files for the project
    Added all of the initial files for the project

---
+++ Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class PlayerController : CharacterBody2D
+{
+	[Export] public float MoveSpeed = 300.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Get the input direction as vector
+		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		
+		//apply input direction to velocity, multiplied by move speed
+		Velocity = direction * MoveSpeed;
+		MoveAndSlide();
+	}
+}
